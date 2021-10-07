2fc0e4741a57826263e09300203fdbe9ae492df2
turbokalle <lukas.kaluscha@gmail.com>
2021-10-07 11:57:23 +0200

    initial commit

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,61 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+import de.htwg.se.Carcassonne._
+import com.google.inject.{Guice, Injector}
+import de.htwg.se.Carcassonne.aview.gui.StartGUI
+import de.htwg.se.Carcassonne.aview.tui._
+import de.htwg.se.Carcassonne.controller.controllerComponent.ControllerInterface
+import de.htwg.se.Carcassonne.model.playfieldComponent.PlayfieldInterface
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  val injector: Injector = Guice.createInjector(new CarcassonneModule)
+  val controller: ControllerInterface = injector.getInstance(classOf[ControllerInterface])
+  val tui = new TUI(controller)
+  // val gui = new StartGUI(controller)
+
+  /**
+   * Create an Action to render an HTML page.
+   *
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+
+  def newGame = Action { request =>
+    controller.newGame()
+    Ok(views.html.newGame())
+  }
+
+  def createGrid(size: Int) = Action { implicit request =>
+    controller.createGrid(size.toInt)
+    Ok(views.html.addPlayers())
+  }
+
+  def addPlayer(player: String) = Action {
+    controller.addPlayer(player)
+    Ok(views.html.addPlayer())
+  }
+
+  def startGame() = Action {
+    controller.firstCard()
+    Ok(views.html.Application.gameBoard(controller.getPlayfield))
+  }
+
+  def gameBoard() = Action {
+    Ok(views.html.Application.gameBoard(controller.getPlayfield))
+  }
+
+}
---
+++ build.sbt
@@ -0,0 +1,22 @@
+name := """play-carcassonne"""
+organization := "de.htwg.webapp"
+
+version := "1.0-SNAPSHOT"
+scalaVersion := "2.12.2"
+
+lazy val branch = "SE-Final"
+lazy val carcassonne = RootProject(uri("git://github.com/turboka11e/de.htwg.se.Carcassonne.git#%s".format(branch)))
+
+lazy val root = Project("play-carcassonne", file("."))
+  .enablePlugins(PlayScala)
+  .aggregate(carcassonne)
+  .dependsOn(carcassonne)
+
+libraryDependencies += guice
+libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
+
+// Adds additional packages into Twirl
+//TwirlKeys.templateImports += "de.htwg.webapp.controllers._"
+
+// Adds additional packages into conf/routes
+// play.sbt.routes.RoutesKeys.routesImport += "de.htwg.webapp.binders._"
