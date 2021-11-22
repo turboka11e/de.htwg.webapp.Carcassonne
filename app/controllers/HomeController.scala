a35e331e3b00cc551d257a5fce559591a54b8620
turbokalle <lukas.kaluscha@gmail.com>
2021-11-22 18:35:32 +0100

    implemented ajax and jquery for gameBoard

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,6 +3,7 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
 
 import de.htwg.se.Carcassonne._
 import com.google.inject.{Guice, Injector}
@@ -11,24 +12,23 @@ import de.htwg.se.Carcassonne.aview.tui._
 import de.htwg.se.Carcassonne.controller.controllerComponent.ControllerInterface
 import de.htwg.se.Carcassonne.model.playfieldComponent.PlayfieldInterface
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+/** This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject() (val controllerComponents: ControllerComponents)
+    extends BaseController {
 
   val injector: Injector = Guice.createInjector(new CarcassonneModule)
-  val controller: ControllerInterface = injector.getInstance(classOf[ControllerInterface])
+  val controller: ControllerInterface =
+    injector.getInstance(classOf[ControllerInterface])
   val tui = new TUI(controller)
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+  /** Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method will be
+    * called when the application receives a `GET` request with a path of `/`.
+    */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
@@ -50,12 +50,28 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
 
   def startGame() = Action {
     controller.firstCard()
-    Ok(views.html.Application.gameBoard(controller.getPlayfield, manicanList()))
+    Redirect(routes.HomeController.gameBoard())
   }
 
   def gameBoard() = Action {
-    val cards = List("D", "E", "G", "H", "I", "J", "K", "L", "N", "O", "R", "T", "U", "V", "C", "W")
-    
+    val cards = List(
+      "D",
+      "E",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "N",
+      "O",
+      "R",
+      "T",
+      "U",
+      "V",
+      "C",
+      "W"
+    )
     Ok(views.html.Application.gameBoard(controller.getPlayfield, manicanList()))
   }
 
@@ -66,16 +82,22 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
     var manicanList: List[(String, String)] = Nil
 
     if (!freshCardAreas.exists(p => p.player != -1)) {
-        for (x <- freshCardAreas.indices) {
-            val selectDir: String = dirCombi.find(p => p.startsWith(freshCardAreas(x).corners.head.toString)).get
-            freshCardAreas(x).value match {
-            case 'c' => manicanList = (selectDir, "manicanEmpty") :: manicanList
-            case 'r' => manicanList = (selectDir, "manicanEmpty") :: manicanList
-            case 'g' =>
-          }
+      for (x <- freshCardAreas.indices) {
+        val selectDir: String = dirCombi
+          .find(p => p.startsWith(freshCardAreas(x).corners.head.toString))
+          .get
+        freshCardAreas(x).value match {
+          case 'c' => manicanList = (selectDir, "manicanEmpty") :: manicanList
+          case 'r' => manicanList = (selectDir, "manicanEmpty") :: manicanList
+          case 'g' =>
         }
+      }
     } else {
-      val dir = controller.getPlayfield.freshCard.card.areas.find(p => p.player != -1).get.corners.head
+      val dir = controller.getPlayfield.freshCard.card.areas
+        .find(p => p.player != -1)
+        .get
+        .corners
+        .head
       val combi = dirCombi.find(p => p.startsWith(dir.toString)).get
       val playerManican = "manican" + controller.getPlayfield.isOn
       manicanList = (combi, playerManican) :: manicanList
@@ -83,37 +105,78 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
     manicanList
   }
 
-  def selectArea(dir: String, x: Int, y: Int) = Action {
-
-    dir match {
-      case "north" => putManican('n')
-      case "south" => putManican('s')
-      case "east" => putManican('e')
-      case "west" => putManican('w')
-    }
-    
-    Ok(views.html.Application.gameBoard(controller.getPlayfield, manicanList()))
+  def selectArea() = Action { implicit request =>
+    val json = request.body.asJson
+    json
+      .map { json =>
+        val dir = (json \ "dir").as[String];
+        println("Selected Manican: " + dir);
+        dir match {
+          case "north" => putManican('n')
+          case "south" => putManican('s')
+          case "east"  => putManican('e')
+          case "west"  => putManican('w')
+        }
+        Ok(
+          views.html.Application
+            ._freshCard(controller.getPlayfield, manicanList())
+        ).as(HTML)
+      }
+      .getOrElse {
+        InternalServerError("Error")
+      }
   }
 
   def putManican(dir: Char): Unit = {
-        val area = controller.getPlayfield.freshCard.card.getAreaLookingFrom(dir)
-        val index = controller.getPlayfield.freshCard.card.areas.indexWhere(p => p.eq(area))
-        controller.selectArea(index)
-    }
-
-  def placeCard(row: Int, col: Int, x: Int, y: Int) = Action {
-    controller.placeCard(row, col)
-    Ok(views.html.Application.gameBoard(controller.getPlayfield, manicanList()))
+    val area = controller.getPlayfield.freshCard.card.getAreaLookingFrom(dir)
+    val index =
+      controller.getPlayfield.freshCard.card.areas.indexWhere(p => p.eq(area))
+    controller.selectArea(index)
   }
 
-  def rotate(dir: String) = Action {
-    if (dir.equals("Left")) {
-      controller.rotateLeft
-    }
-    if (dir.equals("Right")) {
-      controller.rotateRight
-    }
-    Ok(views.html.Application.gameBoard(controller.getPlayfield, manicanList()))
+  def placeCard() = Action { implicit request =>
+    val json = request.body.asJson
+    json
+      .map { json =>
+        val row = (json \ "row").as[Int];
+        val col = (json \ "col").as[Int];
+        println("Updated: Row " + row + " Col " + col);
+        controller.placeCard(row, col)
+
+        var respond = JsObject(
+        "tile" -> JsString(views.html.Application._tile(controller.getPlayfield, manicanList(), row, col).toString) ::
+        "freshCard" -> JsString(views.html.Application._freshCard(controller.getPlayfield, manicanList()).toString) ::
+        "stats" -> JsString(views.html.Application._stats(controller.getPlayfield).toString) ::
+          Nil);
+        Ok(respond).as(JSON)
+      }
+      .getOrElse {
+        InternalServerError("Error")
+      }
+  }
+
+  def rotate() = Action { implicit request =>
+    val json = request.body.asJson
+
+    json
+      .map { json =>
+        val dir = (json \ "dir").as[String];
+        println(dir);
+        if (dir.equals("Left")) {
+          controller.rotateLeft
+        }
+        if (dir.equals("Right")) {
+          controller.rotateRight
+        }
+        Ok(
+          views.html.Application
+            ._freshCard(controller.getPlayfield, manicanList())
+        ).as(HTML)
+      }
+      .getOrElse {
+        println(request.body)
+        Ok("error")
+      }
   }
 
   def rules() = Action {
---
+++ public/javascripts/boardgame.js
@@ -0,0 +1,130 @@
+const delay = millis => new Promise((resolve, reject) => {
+    setTimeout(_ => resolve(), millis)
+});
+
+const rotateImage = function (calcRotation, dir) {
+    let readIn = document.getElementById('cardP').getAttribute('rotation');
+    let current_rotation = parseInt(readIn);
+    var manicans = document.getElementsByName("manicans");
+
+    for (let item of manicans) {
+        item.style.visibility = "hidden";
+    }
+
+    var elem = document.getElementById("cardP");
+    current_rotation = calcRotation(current_rotation);
+
+    elem.style.transform = "rotate(" + current_rotation + "deg)";
+    Promise.all(
+        elem.getAnimations().map(
+            function (animation) {
+                return animation.finished
+            }
+        )
+    ).then(
+        function () {
+            ajaxRotate(dir)
+            return true;
+        }
+    )
+
+    return false;
+}
+
+$(document).on('submit', 'form#rightButtonForm', (e) => {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    rotateImage((rot) => { return rot + 90 }, "Right")
+});
+
+$(document).on('submit', 'form#leftButtonForm', (e) => {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    rotateImage((rot) => { return rot - 90 }, "Left")
+});
+
+var ajaxRotate = function (dir) {
+    let r = {
+        "dir": dir
+    };
+    $.ajax({
+        url: '/rotate',
+        type: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify(r),
+        async: true,
+        success: function (data) {
+            $('div#freshCard').html(data);
+        },
+        error: function (e) {
+            console.log(e);
+        }
+    });
+    return false;
+};
+
+var ajaxPlaceCard = function (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+
+    console.log( $( this ).attr( "id" ));
+    let id = $( this ).attr( "id" );
+
+    let request = {
+        "row": parseInt($( this ).attr( "row" )),
+        "col": parseInt($( this ).attr( "col" )),
+    };
+
+    $.ajax({
+        url: '/placeCard',
+        type: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify(request),
+        async: true,
+        success: function (data) {
+            $('form#' + id).html(data.tile);
+            $('div#freshCard').html(data.freshCard);
+            $('ul#stats').html(data.stats);
+        },
+        error: function (e) {
+            console.log(e);
+        }
+    });
+    return false;
+};
+
+$(document).on('submit', 'form.tile', ajaxPlaceCard);
+
+var ajaxManican = function (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+
+    console.log( "manican " + $( this ).attr( "dir" ));
+
+    let request = {
+        "dir": $( this ).attr( "dir" )
+    };
+
+    $.ajax({
+        url: '/selectArea',
+        type: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify(request),
+        async: true,
+        success: function (data) {
+            $('div#freshCard').html(data);
+        },
+        error: function (e) {
+            console.log(e);
+        }
+    });
+    return false;
+};
+
+$(document).on('submit', 'form.manican', ajaxManican);
